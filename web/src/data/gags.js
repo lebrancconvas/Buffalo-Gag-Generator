75c2016c18a4b5bc6dde29c8b3666b1adcf24beb
lebrancconvas <lebrancconvas@gmail.com>
2021-12-05 02:29:03 +0700

    🗃️ Add Gags Data.

---
+++ web/src/data/gags.js
@@ -0,0 +1,15 @@
+const gags = [{
+    "id": 1,
+    "content": "มีนาแล้ว มีใจให้เราบ้างยัง",
+    "resource": "Internet"
+}, {
+    "id": 2,
+    "content": "ปลาอะไรอร่อย เฉลยปลาปริก้า",
+    "resource": "Internet"
+}, {
+    "id": 3,
+    "content": "ประเทศอะไรชาวนาเมา เฉลย ประเทศนามีเบีย",
+    "resource": "รายการมุกควายเครียด"
+}]
+
+export default gags;
